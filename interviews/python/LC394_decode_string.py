9eb1886c469a235cc86ef08a31741984eb4dfec9
Yibo Zhuang <yibzhuang@gmail.com>
2019-07-21 22:41:35 -0700

    leetcode problems

---
+++ interviews/python/LC394_decode_string.py
@@ -0,0 +1,18 @@
+def decodeString(s):
+    stack = []
+    stack.append(["", 1])
+    num = ""
+    for ch in s:
+        if ch.isdigit():
+            num += ch
+        elif ch == '[':
+            stack.append(["", int(num)])
+            num = ""
+        elif ch == ']':
+            st, k = stack.pop()
+            stack[-1][0] += st*k
+        else:
+            stack[-1][0] += ch
+    return stack[0][0]
+
+print(decodeString("3[a]2[bc]"))
